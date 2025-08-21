0304e38375721ed4626ce54316008798523fea91
Frank <fligori@gmail.com>
2025-08-20 20:04:56 -0400

    all done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-	  site: 'https://batman978.github.io',
-	  base: '/astro',
+  site: 'https://batman978.github.io',
+  base: '/astro',
+  integrations: [preact()],
 });
